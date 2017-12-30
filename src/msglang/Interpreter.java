299528036f8aad54b50f3705cc32d54677958084
Hridesh Rajan <hridesh@gmail.com>
2017-12-30 14:08:31 -0600

    more robust frontend

---
+++ src/msglang/Interpreter.java
@@ -0,0 +1,43 @@
+package msglang;
+import java.io.IOException;
+
+import msglang.Env;
+import msglang.Value;
+import msglang.AST.*;
+
+/**
+ * This main class implements the Read-Eval-Print-Loop of the interpreter with
+ * the help of Reader, Evaluator, and Printer classes. 
+ * 
+ * @author hridesh
+ *
+ */
+public class Interpreter {
+	public static void main(String[] args) {
+		System.out.println("ActLang: Type a program to evaluate and press the enter key,\n" + 
+				"e.g. (ref 342) \n" + 
+				"or try (deref (ref 342)) \n" +
+				"or try (let ((class (ref 342))) (deref class)) \n" +
+				"or try (let ((class (ref 342))) (set! class 541)) \n" + 
+				"or try  (let ((r (ref 342))) (let ((d (free r))) (deref r))) \n" +
+				"Press Ctrl + C to exit.");
+		Reader reader = new Reader();
+		Evaluator eval = new Evaluator(reader);
+		Printer printer = new Printer();
+		REPL: while (true) { // Read-Eval-Print-Loop (also known as REPL)
+			Program p = null;
+			try {
+				p = reader.read();
+				if(p._e == null) continue REPL;
+				Value val = eval.valueOf(p);
+				printer.print(val);
+			} catch (Env.LookupException e) {
+				printer.print(e);
+			} catch (IOException e) {
+				System.out.println("Error reading input:" + e.getMessage());
+			} catch (NullPointerException e) {
+				System.out.println("Error:" + e.getMessage());
+			}
+		}
+	}
+}
